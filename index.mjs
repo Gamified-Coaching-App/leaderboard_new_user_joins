7e7ca5b287514a8f8302d047844e6e4eb29d0153
mariashapo <mart99maria@gmail.com>
2024-03-05 15:51:10 +0000

    Go back to the old version for debuggin

--- index.mjs
+++ index.mjs
@@ -1,12 +1,10 @@
 import AWS from 'aws-sdk';
-import https from 'https'; 
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const handler = async (event) => {
     // cognito trigger/trigger from update in user_id db
     const user_id = event.request.userAttributes.sub;
-    console.log("User id:", user_id);
     //const username = event.request.userAttributes;
 
     // Define the parameters for querying the position_new column
@@ -49,66 +47,12 @@ export const handler = async (event) => {
 
         // Add the new row to the leaderboard table
         await dynamoDb.put(putParams).promise();
+
         console.log('Successfully added new user to leaderboard');
-        try {
-            await triggerChallengesNewUserGeneration(user_id);
-            console.log(`Challenges generation triggered for new user with ID: ${user_id}`);
-        } catch (error) {
-            console.error("Failed to trigger challenges for new user generation:", error);
-        }
 
     } catch (err) {
         console.error('Error adding new user to leaderboard:', err);
     }
 
     return event;
-}
-
-async function triggerChallengesNewUserGeneration(userId) {
-    console.log(`Triggering challenge generation for new user: ${userId}`);
-
-    const challengesApiUrl = 'https://jkipopyatb.execute-api.eu-west-2.amazonaws.com/dev/challenge-creation-new-user';
-
-    // Prepare the payload
-    const apiPayload = {
-        user_id: userId, 
-        season_id: `new_user_season`
-    };
-
-    // Convert the payload to a string
-    const dataString = JSON.stringify(apiPayload);
-
-    // Define the options for the HTTPS request
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(dataString),
-        },
-    };
-
-    // Return a new Promise that resolves or rejects based on the API call outcome
-    return new Promise((resolve, reject) => {
-        const req = https.request(challengesApiUrl, options, (res) => {
-            let response = '';
-
-            res.on('data', (chunk) => {
-                response += chunk;
-            });
-
-            res.on('end', () => {
-                console.log("Response from challenges_new_user_generation:", response);
-                resolve(response); // Resolve the promise with the response
-            });
-        });
-
-        req.on('error', (e) => {
-            console.error("Error calling challenges_new_user_generation:", e);
-            reject(e); // Reject the promise on error
-        });
-
-        // Send the request with the payload
-        req.write(dataString);
-        req.end();
-    });
 }
